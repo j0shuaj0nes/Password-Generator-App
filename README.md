cabeb3027b1e7b92ebfd3df7157da3ea7c7d4ade
Josh Jones <9joshuajones5@gmail.com>
2023-11-02 23:06:58 +0800

    updated javascript

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
 # Password-Generator-App
-Random Password Generator 
+
+## Description
+
+
+The drive behind this project was to create an application that would generate a password based on a user's personal preference and requirements. Random password generators enhance security and help protect senstive data. Thus the rationale behind building this application was to do just that, elevate the protection of sensitive data by generating strong passwords. 
+Users have the ability to set their password length and which character types are included based on their own needs or preference. 
+
+Building this app expanded my understanding of the capabilities of JavaScript. Having only been introduced to JS one week ago, I now see the fantastic functionality it can bring to any webpage, site or app. 
+
+
+## Usage
+
+To use this application, start by clicking on the Generate Password button, as shown below, to select the password length.
+Answer the prompt with 'Ok' for yes, or 'Cancel' for no.
+
+![Generate Password](../Password-Generator-App/Assets/images%20/Screenshot-pw-length.png)
+
+Then follow through the prompts as shown below, again answering 'Ok' for yes, and 'Cancel' for no. 
+
+![Lowercase Option](../Password-Generator-App/Assets/images%20/Screenshot-lowercase.png)
+![Uppercase Option](../Password-Generator-App/Assets/images%20/Screenshot-uppercase.png)
+![Numeric Option](../Password-Generator-App/Assets/images%20/Screenshot-numeric.png)
+![Special Character Option](../Password-Generator-App/Assets/images%20/Screenshot-specialC.png)
+
+Once all criteria have been chosen, a password will generate in the box bordered with a dashed line. 
+
+If no criteria are selected, the following message will be prompted, see below. 
+
+![Option Must Be Selected](../Password-Generator-App/Assets/images%20/Screenshot-must-option-select.png)
+
+## Deployed Web App 
+
--- script.js
+++ script.js
@@ -1,6 +1,38 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Create 4 variables, one for each of the password types. Ex: lowercase will have abcdef.....
+function generatePassword() {
+  var passwordLength = parseInt(window.prompt("Select Password Length between 8 and 128 Characters"))
+  
+  if(typeof passwordLength !== 'number' || passwordLength < 8 || passwordLength > 128){
+    alert("Password must be greater than 8 characteers or less than 128 characters")
+    return;
+  }
 
+  var passwordType_lowercase = window.confirm("Do you want to include lowercase letters in your password?")
+  var passwordType_Uppercase = window.confirm("Do you want to include uppercase letters in your password?")
+  var passwordType_Numeric = window.confirm("Do you want to include numeric vales in your password?")
+  var passwordType_specialcharacters = window.confirm("Do you want to include special characters in your password?")
+
+  if(!passwordType_lowercase && !passwordType_Uppercase && !passwordType_Numeric && !passwordType_specialcharacters){
+    alert ("One option must be selected") 
+    return;
+  }
+  
+  var numericCharacters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+  var specialCharacters = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
+  var lowerCasedCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  var upperCasedCharacters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  
+ combinedcharacters()
+ 
+  
+  // Next Steps:
+  // Create a variable called password Bank.
+  // Look through the different passwordTypes from the user, The ones that are true, add all of those types to the character bank
+  // Look through character bank to generate passworld that is the length that the user wants
+  // Return that password.
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
